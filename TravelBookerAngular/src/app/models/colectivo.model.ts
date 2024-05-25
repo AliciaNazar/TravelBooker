838bd0ac0e6f20f54fae13c81b12e44a2579092e
AliciaNazar <alicianazarab@gmail.com>
2024-05-25 16:03:54 -0300

    cambio en el modelo de colectivo

--- TravelBookerAngular/src/app/models/colectivo.model.ts
+++ TravelBookerAngular/src/app/models/colectivo.model.ts
@@ -2,8 +2,4 @@ export interface IColectivo {
     id:           number;
     matricula:    string;
     totalButacas: number;
-<<<<<<< HEAD
-=======
-    completo:     boolean; //BORRAR!
->>>>>>> e869578d8ea772c899111e2359066bfcd0b4d222
 }
